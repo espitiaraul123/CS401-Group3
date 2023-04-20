133f88bd649a73a631d52f9ecbc78e220e3b7bb7
Sediq1 <sediqaziz@ymail.com>
2023-04-20 14:39:26 -0700

    Update ATM and Transaction class
    Updated ATM and Transaction class with comments

--- ATM.java
+++ ATM.java
@@ -5,7 +5,6 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
 public class ATM {
--- Transaction.java
+++ Transaction.java
@@ -1,38 +1,45 @@
-public class Transaction {
+ public class Transaction {
 	private int transactionID;
 	private double amount;
 	private TransactionType type;
 	
-	public Transaction(int id, double amount, TransactionType type) {
-		this.transactionID = transactionID;
-		this.amount = amount;
-		this.type = type;
+// Constructor to initialize transactionID, amount and type
+public Transaction(int id, double amount, TransactionType type) {
+	this.transactionID = transactionID;
+	this.amount = amount;
+	this.type = type;
 	}
 	
+	// Getter method to return transactionID
 	public int getId() {
 		return transactionID;
 	}
 	
+	// Getter method to return transaction amount
 	public double getAmount() {
 		return amount;
 	}
 	
+	// Getter method to return transaction type
 	public TransactionType getType() {
 		return type;
 	}
 	
+	// This method saves a list of transactions to a file
 	public static void saveTransactions(List<Transaction> transactions, Strig fileName) {
 		try {
 			FileWriter filename = new FileWriter(fileName);
+			// Loop through the transactions and write them to the file
 			for (Transaction t: transactions) {
 				writer.write(t.toString() + "\n");
 			}
 			filename.close();
 		} catch (IOException e) {
-			System.out.println("An error occured while writing the transaction to the file.");
+			System.out.println("An error occurred while writing the transaction to the file.");
 		}
 	}
 	
+	// This method returns a string representation of the transaction
 	@Override
 	public String toString() {
 		String transactionType = "";
@@ -43,10 +50,12 @@ public class Transaction {
 		case WITHDRAW:
 			transactionType = "Withdraw";
 			break;
-		case TRANSFER:
+		case TRANSFER_IN:
 			transactionType = "Transfer";
 			break;
 		}
+		
+		// return a formatted string with transaction ID, type, and amount
 		return String.format("ID: %d, %s: %.2f", id, transactionType, amount);
-	}
+	 }
 }
