caddf1669781ca139b90e06c042979b192a462d8
espitiaraul123 <espitiaraul123@gmail.com>
2023-04-24 21:56:00 -0700

    Client Skeleton - Raul Espitia

--- Client.java
+++ Client.java
@@ -1,1 +1,38 @@
+import java.io.*;
+import java.net.*;
 
+// Client class
+class Client {
+	// driver code
+	public static void main(String[] args) throws ClassNotFoundException {
+		// establish a connection by providing host and port number
+		try (Socket socket = new Socket("localhost", 1234)) {
+	        // create a ObjectOutputStream so we can write data from it.
+	        ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+	        // create a ObjectInputStream so we can read data from it.
+	        ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());
+
+	        int choice = 1;
+	        do {
+	        	Message message = null;
+	        	Message returnMessage = null;
+	        	switch (choice) {
+	        	case 1:
+	        		message = new Message();
+	        		objectOutputStream.writeObject(message);
+	        		returnMessage = (Message)objectInputStream.readObject();
+	        		System.out.println(returnMessage.getText());
+	        		break;
+	        	case 2:
+	        		break;
+	        	case 3:
+	        		break;
+	        	default:		
+	        	}
+	        } while (choice != 1);
+	        socket.close();
+		}
+		catch (IOException e) {
+		}
+	}
+}
