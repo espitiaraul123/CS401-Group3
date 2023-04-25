41cdfc1fe1aebb71f8d676a77c4a04cc2098bc2e
espitiaraul123 <73984984+espitiaraul123@users.noreply.github.com>
2023-04-24 21:30:33 -0700

    New Server Skeleton - Raul Espitia

--- Server.java
+++ Server.java
@@ -1,58 +1,66 @@
 import java.io.*;
-import java.net.Socket;
+import java.net.*;
+
+import java.io.File;  // Import the File class
+import java.io.IOException;  // Import the IOException class to handle errors
+
 
 public class Server {
-	private ServerSocket serverSocket;
-	
-	//Constructor that creates a ServerSocket listening on a specific port
-	public Server(int port) throws IOException {
-		serverSocket = new ServerSocket(port);
-	}
-	
-	//Start method that accepts incoming client connections and creates new thread for each client
-	public void start() throws IOException {
-		while (true) {
-			Socket clientSocket = serverSocket.accept();
-			String ipAddress = clientSocket.getInetAddress().getHostAddress();
-			int port = clientSocket.getPort();
-			System.out.println("Connection established with " + ipAddress + ", port = " + port);
-			new Thread(new ClientHandler(clientSocket)).start();
+	public static void main(String[] args) {
+		ServerSocket server = null;
+
+		try {
+			// server is listening on port 1234
+			server = new ServerSocket(1234);
+			server.setReuseAddress(true);
+			
+			System.out.println("Server Started");
+			
+			// Infinite loop accepts incoming client connections and creates new thread for each client
+			while (true) {
+				Socket client = server.accept(); 					// socket object to receive incoming client requests
+				ClientHandler clientSock = new ClientHandler(client);	// create a new thread object
+				new Thread(clientSock).start();							// This thread will handle the client separately
+			}
 		}
-	}
-	
-	//Client Handler class that handles communication with a single client
-	class ClientHandler implements Runnable {
-		private Socket clientSocket;		//Socket connected to the client
-		private ObjectOutputStream out;		//Output stream for sending messages
-		private ObjectInputStream in;		//Input stream for receiving messages
-		
-		//Constructor that initializes the socket and input/output streams
-		public ClientHandler(Socket socket) throws IOException {
-			this.clientSocket = socket;
-			out = new ObjectOutputStream(clientSocket.getOutputStream());
-			in = new ObjectInputStream(clientSocket.getInputStream());
+		catch (IOException e) {
 		}
-		
-		//Run method that runs a new thread and handles communication with client
-		public void run() {
-			try {
-				//Wait for login message from client
-				(blank) message = (blank) in.readObject();
-				
-				//If the login message is received, send a confirmation response and start messasge looping
-				if (message.getType().equals("login") ) {
-					message.setStatus("success");
-					out.writeObject(message);
-					out.flush();
-				} else if (message.getType().equals("logout")) {
-					message.setStatus("success");
-					out.writeObject(message);
-					out.flush();
-					clientSocket.close();
-					break;
+		finally {
+			if (server != null) {
+				try {
+					server.close();
+				}
+				catch (IOException e) {
 				}
 			}
 		}
 	}
+	
+    private static class ClientHandler implements Runnable {
+        private Socket socket;
 
+        ClientHandler(Socket socket) {
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+			Message message = null;
+			//boolean connection = false;
+			
+			try {
+		        // create a ObjectOutputStream so we can write data from it.
+		        ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+		        // create a ObjectInputStream so we can read data from it.
+		        ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());
+
+		       while((message = (Message)objectInputStream.readObject()) != null) {
+		    	   // do stuff
+		    	   message.setText("POOP");
+		    	   objectOutputStream.writeObject(message);
+		       }	
+			} catch (Exception e){
+			}	
+        }
+    } 
 }
