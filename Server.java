a9368525abe7ffcd4b717dd8bcea60773d54993a
espitiaraul123 <espitiaraul123@gmail.com>
2023-04-26 18:18:21 -0700

    readCustomerFromFile Added - Raul Espitia
    readCustomerFromFile works how it is, and can read data from a text file as is explained in the comments. We expect to make updates to it though.

--- Server.java
+++ Server.java
@@ -1,9 +1,7 @@
 import java.io.*;
 import java.net.*;
-
-import java.io.File;  // Import the File class
+import java.util.*;
 import java.io.IOException;  // Import the IOException class to handle errors
-
 
 public class Server {
 	public static void main(String[] args) {
@@ -48,19 +46,121 @@ public class Server {
 			Message message = null;
 			//boolean connection = false;
 			
+        	Map<Integer, Customer> customers = new HashMap<>();
+			
 			try {
 		        // create a ObjectOutputStream so we can write data from it.
-		        ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+		        // ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
 		        // create a ObjectInputStream so we can read data from it.
 		        ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());
 
-		       while((message = (Message)objectInputStream.readObject()) != null) {
-		    	   // do stuff
-		    	   message.setText("POOP");
-		    	   objectOutputStream.writeObject(message);
-		       }	
-			} catch (Exception e){
-			}	
+		        while((message = (Message)objectInputStream.readObject()) != null) {
+		        	// do stuff
+		        }
+			} catch (Exception e) {
+				
+			}
+        }
+        
+        /* NOT TESTED YET
+        public void writeCustomersToFile(Map<Integer, Customer> customers) {
+        	try {
+        		FileWriter customerDataFile = new FileWriter("customerData.txt");
+        		BufferedWriter fileWriter = new BufferedWriter(customerDataFile);
+        		
+        		for (Map.Entry<Integer, Customer> customer : customers.entrySet()) {
+        			fileWriter.write(customer.toString());
+        		}
+        		fileWriter.close();
+        	} catch (IOException e) {
+        		e.printStackTrace();
+        	}
+        }
+		*/
+
+        public void readCustomersFromFile(Map<Integer, Customer> customers) {
+        	try {
+        		// Open file that holds all the data for every customer.
+        		// Example of file structure:
+        		// customer 1
+        		// account 1
+        		// transaction 1
+        		// transaction 2
+        		// transaction 3
+        		// account 2
+        		// transaction 1
+        		// transaction 2
+        		// transaction 3
+        		// customer 2
+        		// etc.
+        		// Each piece of data is separated by a comma.
+        		FileReader customerDataFile = new FileReader("customerData.txt");
+        		BufferedReader fileReader = new BufferedReader(customerDataFile);
+        		
+        		// Read every line of the file, while organizing everything to a customer object
+        		String line = fileReader.readLine();
+        		while (line != null) {
+        			// Splits up the line of text, separating everything by commas
+        			String[] customerParts = line.split(",");
+        			
+        			// organize the data into variables (just for readability)
+        			int userID = Integer.parseInt(customerParts[0]);
+        			int PIN = Integer.parseInt(customerParts[1]);
+        			String name = customerParts[2];
+        			String address = customerParts[3];
+        			int numAccounts = Integer.parseInt(customerParts[4]);
+        			
+        			// Get all the accounts the customer has
+        			List<Account> accounts = new ArrayList<>();
+        			for (int i = 0; i < numAccounts; i++) {
+        				// Remember every object is in its own line
+        				line = fileReader.readLine();
+        				// Splits up the line of text, separating everything by commas
+        				String[] accountParts = line.split(",");
+        				
+        				// organize the data into variables (just for readability)
+        				int accountID = Integer.parseInt(accountParts[0]);
+        				double balance = Double.parseDouble(accountParts[1]);
+        				String accountType = accountParts[2];
+        				int numTransactions = Integer.parseInt(accountParts[3]);
+        				
+        				// Get all the transactions the account has
+        				List<Transaction> transactions = new ArrayList<>();
+        				for (int j = 0; j < numTransactions; j++) {
+        					// Remember every object is in its own line
+        					line = fileReader.readLine();
+        					// Splits up the line of text, separating everything by commas
+        					String[] transactionParts = line.split(",");
+        					
+        					// organize the data into variables (just for readability)
+        					int transactionID = Integer.parseInt(transactionParts[0]);
+        					double amount = Double.parseDouble(transactionParts[1]);
+        					String transactionType = transactionParts[2];
+        					String date = transactionParts[3];
+        					
+        					// Create a new transaction object with this data
+        					Transaction transaction = new Transaction(transactionID, amount, transactionType, date);
+        					// Add this transaction object into the list of transactions that this account has
+        					transactions.add(transaction);
+        				}
+        				// Create a new account object with this data
+        				Account account = new Account(accountID, balance, accountType, numTransactions, transactions);
+        				// Add this account object into the list of accounts that this customer has
+        				accounts.add(account);
+        			}
+        			// Create a new customer object with this data
+        			Customer customer = new Customer(userID, PIN, name, address, numAccounts, accounts);
+        			// Add this customer object into the HashMap of customers
+        			customers.put(customer.getUserID(), customer);
+        			
+        			// Repeat!
+        			line = fileReader.readLine();
+        		}
+        		// Close file when done :)
+        		fileReader.close();
+        	} catch (IOException e) {
+        		e.printStackTrace();
+        	}
         }
     } 
 }
