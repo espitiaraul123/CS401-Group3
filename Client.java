36ddd54518d6d993bd70426890f217d7705ee184
spidrman <fernando.e.h02@gmail.com>
2023-04-28 21:04:25 -0700

    Update Client.java
    Made the client compatible with the server and GUI, no more work is needed on this. the rest of the lifting will be done by the Banker and ATM guis

--- Client.java
+++ Client.java
@@ -1,7 +1,9 @@
 import java.io.*;
 import java.net.*;
+import java.util.ArrayList;
 import java.util.Scanner;
 
+import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
 // Client class
@@ -11,91 +13,91 @@ class Client {
 		System.out.println("inside client class");
 		// establish a connection by providing host and port number
 		try (Socket socket = new Socket("localhost", 1234)) {
-			//create way to send data to server
-			PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-			
-			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-			
-			Scanner scanner = new Scanner (System.in);
-			String line = null;
-			
 			//start the GUI
 			System.out.println("starting login GUI");
 			//use show input dialog
 			String username = JOptionPane.showInputDialog(null, "Hello, please enter your username");
 			String password = JOptionPane.showInputDialog(null, "please enter your password");
 			
-			String loginInfo = username+ " "+password;
+			String loginInfo = username+password;
 			
-			//use enteredcredentials and send them to the server
-			/*while (!"exit".equalsIgnoreCase(line)) {
-				System.out.println("credentials have not been made");
-				if (login.enteredCredentials() == true)
-				{
-					System.out.println("credentials have now been entered");
-					break;
-				}
-			}*/
-			System.out.println("veryfying your identity using "+loginInfo+" by first making a message object");
-			Message loginMessage = new Message(MsgType.Login, MsgStatus.Undefined, loginInfo);
+			
+			System.out.println("verifying your identity using your login info");
+			Message loginMessage = new Message();
+			loginMessage.makeLoginMessage(username, password);
 			
 			// create a ObjectOutputStream so we can write data from it.
-	        ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+	        ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
 	        // create a ObjectInputStream so we can read data from it.
-	        ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());
-	        System.out.println("writing object");
-	        
-	        objectOutputStream.writeObject(loginMessage);
-	        System.out.println("fetching return message");
-    		Message returnMessage = (Message)objectInputStream.readObject();
-    		
-    		if (returnMessage.getStatus() == MsgStatus.Success) {
-    			System.out.println("Successful login");
+	        ObjectInputStream ois= new ObjectInputStream(socket.getInputStream());
+	        //passing message to server
+	        oos.writeObject(loginMessage);
+	        System.out.println("fetching message");
+	        //getting message from the server
+    		Message returnMessage = (Message)ois.readObject();
+    		System.out.println("got message");
+    	
+    		if (returnMessage.getStatus() == MsgStatus.Failure) {
+    			//make a popup
+    			   JFrame jFrame = new JFrame();
+    		       JOptionPane.showMessageDialog(jFrame, "login was unsuccessful");
+    		   
     		}
-    		System.out.println(returnMessage.getText());
-			//pass the message
-			out.println(loginMessage);
-			out.flush();
+    		else {
+    			String returnM = returnMessage.username + returnMessage.password;
+    			System.out.println(returnM);
+    		    String s[] = returnM.split(",");
+    		    ///if the return message says that the person login in as a banker, show the banker gui
+    		    ///else show the ATM
+    		    
+    		    
+    		    if (returnM.equals("adminbanker")) {
+    		    	JFrame jFrame = new JFrame();
+        		    JOptionPane.showMessageDialog(jFrame, "login was successful! welcome back banker");
+    		    	BankerGUI guiForBanker = new BankerGUI();
+    		    }
+    		    else {
+    		    	System.out.println(returnM);
+    		    	JFrame jFrame = new JFrame();
+        		    JOptionPane.showMessageDialog(jFrame, "login was successful! Fetching your account");
+        		    //if the person is a banker, show the banker gui
+        		    
+    		    	ATM guiForCustomer = new ATM();
+    		    }
+    		    //the guis will now send and receive messages from the server.
+    		    
+    		    
+    		}
+    		/*///make customer objects and write them to the file
+    		ArrayList<Account> accountArray = new ArrayList<>();
+    		Account account1 = new Account("Daffy Duck", AccountType.Checkings, 20.50);
+    		Account account2 = new Account("Porky Pig", AccountType.Checkings, 85.75);
+    		
+    		accountArray.add(account1);
+    		accountArray.add(account2);
+    		
+    		Customer newCustomer = new Customer(12345, "Daffy Duck", "306 Space Jam Ave.", 2, accountArray);
+    		
+    		ArrayList<Account> acountArray1 = new ArrayList<>();
+    		Account acount1 = new Account("Bugs Bunny", AccountType.Checkings, 100);
+    		Account acount2 = new Account("Yosemite Sam", AccountType.Checkings, 85);
+    		
+    		acountArray1.add(acount1);
+    		acountArray1.add(acount2);
+    		
+    		Customer newCustomer1 = new Customer(54321, "Bugs Bunny", "308 Space Jam Ave.", 2, acountArray1);
+    		ObjectOutputStream oostream = new ObjectOutputStream(new FileOutputStream("CustomerData.txt"));
+    		oostream.writeObject(newCustomer);
+    		oostream.writeObject(newCustomer1);
+    		
+    		oostream.close();*/
+    	
+    		//writing these to the file
+    		//oos.writeObject(newCustomer);
+    		//oos.writeObject(newCustomer1);
+    		
 			
-			//out.flush();
-			/*while(!"exit".equalsIgnoreCase(line)) {
-				line = scanner.nextLine();
-				out.println(line);
-				out.flush();
-				System.out.println("Server replied " + in.readLine());
-			}*/
-			
-			scanner.close();
-			
-	        /*// create a ObjectOutputStream so we can write data from it.
-	        ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
-	        // create a ObjectInputStream so we can read data from it.
-	        ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());
-	        
-	        System.out.println("inside client class again");
-	        ///make the user login using the GUI
-	        LoginGUI loginPage = new LoginGUI();
-	        
-	        int choice = 1;
-	        do {
-	        	Message message = null;
-	        	Message returnMessage = null;
-	        	switch (choice) {
-	        	case 1:
-	        		message = new Message();
-	        		objectOutputStream.writeObject(message);
-	        		returnMessage = (Message)objectInputStream.readObject();
-	        		System.out.println(returnMessage.getText());
-	        		break;
-	        	case 2:
-	        		break;
-	        	case 3:
-	        		break;
-	        	default:		
-	        	}
-	        } while (choice != 1);*/
-			System.out.println("ending client program");
-	        socket.close();
+			socket.close();
 		}
 		catch (IOException e) {
 			e.printStackTrace();
