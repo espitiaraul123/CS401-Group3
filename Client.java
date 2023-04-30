ecf61896a255a923ca2d75e542feef179ad16a8c
spidrman <fernando.e.h02@gmail.com>
2023-04-30 00:02:42 -0700

    Update Client.java
    simplified the file to make it easier to read

--- Client.java
+++ Client.java
@@ -14,7 +14,6 @@ class Client {
 		// establish a connection by providing host and port number
 		try (Socket socket = new Socket("localhost", 1234)) {
 			//start the GUI
-			System.out.println("starting login GUI");
 			//use show input dialog
 			String username = JOptionPane.showInputDialog(null, "Hello, please enter your username");
 			String password = JOptionPane.showInputDialog(null, "please enter your password");
@@ -35,6 +34,7 @@ class Client {
 	        System.out.println("fetching message");
 	        //getting message from the server
     		Message returnMessage = (Message)ois.readObject();
+    		System.out.println("back inside client class");
     		System.out.println("got message");
     	
     		if (returnMessage.getStatus() == MsgStatus.Failure) {
@@ -45,9 +45,7 @@ class Client {
     		}
     		else {
     			String returnM = returnMessage.username + returnMessage.password;
-    			System.out.println(returnM);
-    		    String s[] = returnM.split(",");
-    		    ///if the return message says that the person login in as a banker, show the banker gui
+    			///if the return message says that the person login in as a banker, show the banker gui
     		    ///else show the ATM
     		    
     		    
@@ -59,44 +57,19 @@ class Client {
     		    else {
     		    	System.out.println(returnM);
     		    	JFrame jFrame = new JFrame();
-        		    JOptionPane.showMessageDialog(jFrame, "login was successful! Fetching your account");
+        		    JOptionPane.showMessageDialog(jFrame, "login was successful! Fetching your profile");
         		    //if the person is a banker, show the banker gui
         		    
-    		    	ATM guiForCustomer = new ATM();
+    		    	ATM guiForCustomer = new ATM(returnMessage.newCustomer);
     		    }
     		    //the guis will now send and receive messages from the server.
     		    
+    		    while (socket.isConnected()) {
+    		    	
+    		    }
     		    
     		}
-    		/*///make customer objects and write them to the file
-    		ArrayList<Account> accountArray = new ArrayList<>();
-    		Account account1 = new Account("Daffy Duck", AccountType.Checkings, 20.50);
-    		Account account2 = new Account("Porky Pig", AccountType.Checkings, 85.75);
     		
-    		accountArray.add(account1);
-    		accountArray.add(account2);
-    		
-    		Customer newCustomer = new Customer(12345, "Daffy Duck", "306 Space Jam Ave.", 2, accountArray);
-    		
-    		ArrayList<Account> acountArray1 = new ArrayList<>();
-    		Account acount1 = new Account("Bugs Bunny", AccountType.Checkings, 100);
-    		Account acount2 = new Account("Yosemite Sam", AccountType.Checkings, 85);
-    		
-    		acountArray1.add(acount1);
-    		acountArray1.add(acount2);
-    		
-    		Customer newCustomer1 = new Customer(54321, "Bugs Bunny", "308 Space Jam Ave.", 2, acountArray1);
-    		ObjectOutputStream oostream = new ObjectOutputStream(new FileOutputStream("CustomerData.txt"));
-    		oostream.writeObject(newCustomer);
-    		oostream.writeObject(newCustomer1);
-    		
-    		oostream.close();*/
-    	
-    		//writing these to the file
-    		//oos.writeObject(newCustomer);
-    		//oos.writeObject(newCustomer1);
-    		
-			
 			socket.close();
 		}
 		catch (IOException e) {
