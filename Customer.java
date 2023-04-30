962b5116e47efce658e40e57cc87a83c129ab616
espitiaraul123 <espitiaraul123@gmail.com>
2023-04-30 05:45:52 -0700

    Customer Updated - Raul Espitia
    Updates were needed

--- Customer.java
+++ Customer.java
@@ -1,116 +1,112 @@
-import java.util.ArrayList;
 import java.io.*;
 import java.util.*;
 
 public class Customer {
-
-	private int numberOfAccounts;
+	
+	private int userID;
+	private int PIN;
 	private String name;
-	private int UserID;
-	private int pin;
-	private String address;
-	private String email;
-	private ArrayList<Account> accounts;  // list of transactions
-
-	private static int count = 100001;
-	String name, int numberOfAccounts,
-
-	//Constructor to initialize instance variables
-	public Customer(int pin, String name, String address) {
-		//Setting the Customer's attributes
-		this.numberOfAccounts = 0;
+	private int numAccounts;
+	private List<Account> accounts;
+	
+	// A Customer must be initialized with user input
+	
+	// done
+	// Constructor
+	Customer(int userID, int PIN, String name) {
+		this.userID = userID;
+		this.PIN = PIN;
 		this.name = name;
-		this.UserID = count++;
-		this.pin = pin;
-		this.address = address;
-		this.accounts = null;
+		this.numAccounts = 0;
+		this.accounts = new ArrayList<>();
+	}
+	// done
+	// This constructor is just used for testing.
+	Customer(int userID, int PIN, String name, int numAccounts, List<Account> accounts) {
+		this.userID = userID;
+		this.PIN = PIN;
+		this.name = name;
+		this.numAccounts = numAccounts;
+		this.accounts = accounts;
 	}
 	
-    public Customer(int pin, String name, String address, int numAccounts, ArrayList<Account> accounts) {
-		//Setting the Customer's attributes
-		this.numberOfAccounts = numAccounts;
-		this.name = name;
-		this.UserID = count++;
-		this.pin = pin;
-		this.address = address;
-		this.accounts = accounts;
-    	
-    }
-
+	// done
+	// Getters
+	public int getUserID() {
+		return userID;
+	}
+	public int getPIN() {
+		return PIN;
+	}
+	public String getName() {
+		return name;
+	}
+	public int getNumAccounts() {
+		return numAccounts;
+	}
+	public List<Account> getAccounts() {
+		return accounts;
+	}
 	
-	public void setAccounts(ArrayList<Account> accounts) {
-		this.accounts = accounts;
-		
+	// done
+	// Setters
+	public void setUserID(int userID) {
+		this.userID = userID;
 	}
-
-	//Setter to modify number of accounts
-	public void setNumberOfAccounts(int numberOfAccounts) {
-		this.numberOfAccounts = numberOfAccounts;	
+	public void setPin(int PIN) {
+		this.PIN = PIN;
 	}
-
-	//Setter to modify Customer's name
 	public void setName(String name) {
 		this.name = name;
 	}
-
-	//Setter to modify Customer's UserID
-	public void setUserID(String UserID) {
-		this.UserID = UserID;
+	public void getNumAccounts(int numAccounts) {
+		this.numAccounts = numAccounts;	
 	}
-
-	//Setter to modify Customer's name
-	public void setPin(String pin) {
-		this.pin = pin;
-	}
-
-	//Setter to modify Customer's name
-	public void setAddress(String address) {
-		this.address = address;
-	}
-
-	//setter to set the email address of the customer : contact info
-	public void setEmail(String email) {
-		this.email = email;
-
-	}
-
-
-	// get account info in a toString
-	public String getAllAccountsInfo(){
-		String customerAccounts = "";
-		for(int i = 0; i < accounts.size(); i++) {
-
-			customerAccounts += (accounts.get(i).toString() + "\n");
-
-		}
-
-		return customerAccounts;
-
+	public void setAccounts(ArrayList<Account> accounts) {
+		this.accounts = accounts;
 	}
 	
-	public ArrayList<Account> getAccounts(){
+	// done
+	// toString()
+	// Used for reading and writing to the database(text file)
+	public String toString() {
+		String customerAsString = "";
 		
-		return accounts;
+		customerAsString = customerAsString.concat(Integer.toString(userID) + ","
+													+ Integer.toString(PIN) + ","
+													+ name + ","
+													+ Integer.toString(numAccounts) + "\n");
 		
-	}
-
-
-	// finding the accounts
-	public Account findAccount(int acctId, String name) {
-		Account acctFound = null;
-
-		for(int i = 0; i < accounts.size(); i++) {
-			if(accounts.get(i).getAccountNumber() == acctId && accounts.get(i).getName().equals(name)) {
-
-				acctFound = accounts.get(i);
-				break;
-			}
-
+		// Include all the account information
+		for(int i = 0; i < numAccounts; i++) {
+			Account account = accounts.get(i);
+			customerAsString = customerAsString.concat(account.toString());
 		}
-
-		return acctFound;
+		
+		return customerAsString;
+	}
+	
+	// done
+	// Returns an account, which is asked for by its ID
+	public Account getAccount(int accountID) {
+		Account accountTemp;
+		for(int i = 0; i < numAccounts; i++) {
+			accountTemp = accounts.get(i);
+			if (accountTemp.getAccountID() == accountID) {
+				return accountTemp;
+			}
+		}
+		return null;
+	}
+	
+	// done
+	// Add an account to the customer (used for testing)
+	public void addAccount(Account newAccount) {
+		numAccounts++;
+		accounts.add(newAccount);
 	}
 
+	/* idk yet
 	// add a new account to the under the Customer's name
 	public Account addAccount(double initialDeposit, AccountType type) {
 		Account newAcct = new Account(name, type, initialDeposit);
@@ -136,73 +132,5 @@ public class Customer {
 		accountFrom.transfer(amount, accountTo);
 
 	}
-
-
-	// This method saves a Customer to a file
-	public static void saveCustomer(String fileName) {
-		try {
-			FileWriter filename = new FileWriter(fileName);
-			// Loop through the transactions and write them to the file
-			String customer = toString();
-			writer.write(customer);
-			/*for (Transaction t: transactions) {
-				writer.write(toString());
-			}*/
-			filename.close();
-		} catch (IOException e) {
-			System.out.println("An error occurred while writing the Customer to the file.");
-		}
-	}
-
-
-
-	//Getter to retrieve number of accounts
-	public int getnumberOfAccounts() {
-		return numberOfAccounts;
-	}
-
-
-
-	//Getter to retrieve Customer's name
-	public String getName() {
-		return name;
-	}
-
-
-
-	//Getter to retrieve Customer's UserID
-	public String getUserID() {
-		return UserID;
-	}
-
-
-
-	//Getter to retrieve Customer's name
-	public String getPin() {
-		return pin;
-	}
-	
-	//getter for email: contact info
-	public String getEmail() {
-		return email;
-
-	}
-
-
-	//Getter to retrieve Customer's name
-	public String getAddress() {
-		return address;
-	}
-	
-	public String toString() {
-		
-		String customerAccounts = getAllAccountsInfo();
-		
-		return (pin + "," + name + "," + address + "," + numberOfAccounts +
-				"\n" + customerAccounts);
-	
-		
-	}
-
-
+	*/
 }
