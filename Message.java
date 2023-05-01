31fb3bdb53f9cdbc35a2b9324e94a625a25b3000
espitiaraul123 <espitiaraul123@gmail.com>
2023-04-30 20:26:12 -0700

    Message Class Finished - Raul Espitia

--- Message.java
+++ Message.java
@@ -1,59 +1,42 @@
 import java.io.Serializable;
+import java.util.*;
 
 public class Message implements Serializable {
     protected MsgType type;
     protected MsgStatus status;
-    protected String text;
-    //these variables are to send a new customer
-    public Customer newCustomer;
-    //these variables are to login
-    public String username;
-    public String password;
-    public String[] data;
+    protected List<String> data;
+    
     public Message(){
         this.type = MsgType.Undefined;
         this.status = MsgStatus.Undefined;
-        this.text = "Undefined";
+        this.data = new ArrayList<>();
     }
 
-    public Message(MsgType type, MsgStatus status, String[] text){
+    public Message(MsgType type, MsgStatus status, List<String> data){
         this.type = type;
         this.status = status;
-        this.data = text;
-    }
-    public String[] getData() {
-    	return data;
-    }
-    public void makeLoginMessage(String username, String password) {
-    	type = MsgType.Login;
-    	this.username = username;
-    	System.out.println("setting username to "+username);
-    	this.password = password;
-    	System.out.println("setting password to "+password);
-    	
+        this.data = data;
     }
     
-    private void setType(MsgType type){
-    	this.type = type;
-    }
-   
-    public void setStatus(MsgStatus status){
-    	this.status = status;
-    }
-    
-    public void setText(String text){
-    	this.text = text;
-    }
-    
+    // Getters
     public MsgType getType(){
     	return type;
     }
-
     public MsgStatus getStatus(){
     	return status;
     }
-
-    public String getText(){
-    	return text;
+    public List<String> getData() {
+    	return data;
+    }
+    
+    // Setters
+    private void setType(MsgType type){
+    	this.type = type;
+    }
+    public void setStatus(MsgStatus status){
+    	this.status = status;
+    }
+    public void setText(List<String> data){
+    	this.data = data;
     }
 }
